4cd42a2e4a5b78c5cbed36fc2e197d6bddcf119c
Michael Sun <msun2019@pingry.org>
2017-01-09 14:54:02 -0500

    Added multiple reciever functionality

--- chat-transmitter.py
+++ chat-transmitter.py
@@ -21,15 +21,25 @@ serverSocket.listen(0)          # This says not to keep clients queued up in lin
 
 # The server socket is just here to accept incoming connections. When it does
 # accept one, it creates a new client socket for the actual communication.
-print("Waiting for a client to connect")
-clientSocket, clientAddress = serverSocket.accept()
+print("Waiting for clients to connect")
+
+response = ""
+clientList = [] # List of clients that have connected
+while response != "y":
+	response = raw_input("Type enter to wait for a new client to connect, or type 'y' to stop searching for clients")
+	if response == "y":
+		break
+	else:
+		clientSocket, clientAddress = serverSocket.accept()
+		clientList.append(clientSocket) 
 
 
 # Infinte loop that waits for the user to type a message, then transmits it.
 while True:
   message = raw_input("Enter the next message to send: ")
-  clientSocket.send(message)
-  
+  for client in clientList:
+ 	 client.send(message)
+  	
 # Ideally we should always close our sockets, but currently this program won't
 # because it can only be quit by using ctrl + C.
 clientSocket.close()
