546bb726d51a13906f7944d0becc50e0a806cb8a
Josh Orndorff <admin@joshorndorff.com>
2017-01-13 00:13:54 -0500

    Added lots of error handling code so that sockets are closed cleanly and fewer Ctrl-C's are necessary

--- chat-receiver.py
+++ chat-receiver.py
@@ -1,15 +1,30 @@
 import socket
+import sys
+
+# Create a client socket to communicate with the server
+host = "localhost"
+port = 54321
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host = 'localhost'
-port = 54321
+# Try to connect to the server
+try:
+	sock.connect((host, port))
+	
+except socket.error:
+	print("No server running at {}:{}".format(host, port))
+	print("Aborting.\n")
+	sys.exit(1)
+	
 
-sock.connect((host, port))
 
-
-
-while True:
+# Listen for broadacsts until the server closes the socket
+message = " " # Initialize the message just to get into the loop once
+while len(message) > 0:
   # If message is over 1024 bytes long, it will be broken into parts.
   message = sock.recv(1024)
   print(message)
+
+# Clean up the open sockets
+sock.shutdown(socket.SHUT_RDWR)
+sock.close()
--- chat-transmitter.py
+++ chat-transmitter.py
@@ -1,19 +1,21 @@
-# Here is a basic program file to get us started.
 
-# I think this module may be very helpful for us
 # Documentation is here: https://docs.python.org/3/library/socket.html
 import socket
 
 
-# Initialize the socket
-
-host = 'localhost' # localhost just means "this computer"
-port = 54321      # port can be pretty much any number. This one is easy to remember.
-
 # Create a server socket to accept incoming connections from clients.
+host = 'localhost' # Using localhost only makes the socket visible to client on this system.
+port = 54321         # Port can be pretty much any number. This one is easy to remember.
+
 # AF_INET means address format is IPv4
 # SOCK_STREAM specifies what kind of socket it is. This one is common.
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# This makes the socket reuseable immediately after closing the program
+# TODO I hope we can eventually remove this line after we are sure our sockets
+# are always closed nicely, but maybe it will always be necessary.
+# For now it at least reduces cursing while coding :)
+serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 # Now put that socket to use on the host and port we specified above.
 serverSocket.bind((host, port)) # Double parens are important to make the argument a tuple.
@@ -42,14 +44,16 @@ message = ""
 while message != "/exit":
 	message = raw_input("message> ")
 	for client in clientList:
- 		client.send(message)
-	
-# Close the sockets before exiting
+		client.send(message)
 
 
   	
-# Close all sockets
+# Clean up the open sockets before exiting
 for client in clientList:
+	# Tell client that socket is shutdown for reading and writing
+	client.shutdown(socket.SHUT_RDWR)
+	
+	# Actually close the socket on this end
 	client.close()
 
 serverSocket.close()
