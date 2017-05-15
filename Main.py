pingryiRT/DistributedNetwork

commit 789452ab9b158067bbafdc36949ec6f3078974d8

Removed all interface from Network except receiver. Finally removed printThis.

Main.py
@@ -57,7 +57,7 @@ def validateIP(IP):
 	
 	return True
 				
-	
+######## Functions that could be moved to a separate interface module #####
 
 def connector():
 	""" Prompts user for data to connect to another peer, and establishes the connection.
@@ -68,6 +68,38 @@ def connector():
 	peerPort = getPort()
 
 	myNetwork.connect(peerIP, peerPort)
+
+
+
+def name():
+	"""
+	Gives a peer a human-redable name identifier.
+	The name will be accessible through peer.name
+	
+	Warning. Uses global variable myNetwork."""
+	
+	for peer in list(myNetwork.peerList):
+		print(str(peer) + " " + str(self.peerList.index(peers)))
+	index = int(raw_input("Please enter the index of the peer you would like to name: "))
+	name = raw_input("Please enter the new name: ")
+	self.peerList[int(index)].name = name
+
+
+def approver():
+	"""
+	Moves a peer that has connected to this network instance from the
+	unconfirmedList to peerList, where messages can be sent and received.
+	
+	Warning. Uses global variable myNetwork."""
+	
+	i = 0
+	while i < len(myNetwork.unconfirmedList):
+		peer = myNetwork.unconfirmedList[i]
+		add = raw_input("y/n to add: " + str(peer) + " ").lower()
+		if add == "y":
+			myNetwork.approve(peer)
+		
+		i += 1
 
 
 ################################## MAIN PROGRAM BELOW ##################################
@@ -94,20 +126,20 @@ WorkerThread("receiver", myNetwork).start()
 # Main program loop
 command = None
 while command != "/exit":
-	command = raw_input("Please type your message, or enter a command, '/connect', '/accept', '/name', '/addPort', '/exit' then hit enter:  \n")
+	command = raw_input("Please type your message, or enter a command, '/connect', '/approve', '/name', '/addPort', '/exit' then hit enter: \n")
 
 	if command == "/connect":
 		connector()
-	elif command == "/accept":
-		myNetwork.manualAcceptor()
+	elif command == "/approve":
+		approver()
 	elif command == "/name":
-		myNetwork.name()
+		name()
 	elif command == "/addPort":
 		myNetwork.addPort()
 #	elif command == "/init":
 #		myNetwork.manualInit()
 	else:
-		myNetwork.sender(command)
+		myNetwork.send(command)
 
 
 # Close down the network

Network.py
@@ -19,19 +19,16 @@ class Network(object):
 		"""
 		peerList -- a list of all the current Peer objects, which are used to send and receive 
 		messages
-		Stopper -- used to stop everything
-		printStopper -- used as a poor-man's lock object to control printing output 
+		unconfirmedList -- contains peers that have not been manually accepted by the user
+		and are not yet used to send and receive messages
 		ip -- This hosts own IP address as seen by peers on the network
 		port -- The port on which this host is running a server
 		server -- the server socket object of the network (for other nodes to connect to)
 		A name object can be added to peers as a means of providing a unique id
-		unconfirmedList -- contains peers that have not been manually accepted by the user
-		and are not yet used to send and receive messages
 		"""
 		
 		self.unconfirmedList = []
 		self.peerList = []
-		self.printStopper = False
 		self.ip = ip
 		self.port = port
 		
@@ -39,40 +36,17 @@ class Network(object):
 		self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.server.bind((self.ip, self.port))
 		self.server.listen(0)
-      	
-	
-	def printThis(self, message, type = None):
-		"""
-		Very simple method -- if type is none it prints message, otherwise it returns 
-		raw user input with message being used as a prompt
-		"""
-		if type == None:
-			print(message)
-		else:
-			return raw_input(message)
 	
 	
 	
-	def name(self):
-		"""
-		Gives a peer a unique name identifier (determined by the input of the user)
-		The name will be accessible through peer.name
-		"""
-		for peers in list(self.peerList):
-			print(str(peers) + " " + str(self.peerList.index(peers)))
-		index = self.printThis("Please enter the index of the peer you would like to name: \n", type = "input")
-		name = self.printThis("Please enter the name of the peer you would like to name: \n", type = "input")
-		self.peerList[int(index)].name=name
-	
-	
-	
-	def sender(self, sendMessage):
+	def send(self, message):
 		"""
 		Sends message to all peers with a socket
 		"""
+		
 		for peers in list(self.peerList):
 			if peers.hasSock == True: # To me: don't you dare change this to if peers.hasSock: actually this one should work but still...
-				peers.send(sendMessage)
+				peers.send(message)
 
 	
 	def connect(self, ip, port):
@@ -84,30 +58,24 @@ class Network(object):
 			sock.connect((ip, port))
 			
 		except socket.error:
-			self.printThis("Couldn't connect to peer " + str((ip, port)))
+			raise Exception("Couldn't connect to peer " + str((ip, port)))
 		
 		finally:
-			newPeer = Peer(ip, port)
+			newPeer = Peer(ip, port, socket = sock)
 			self.peerList.append(newPeer)
-			newPeer.addSock(sock)
-			self.printThis("connected to " + ip)
 			
 			newPeer.send(self.port)
 	
 	
 	
-	def manualAcceptor(self):
+	def approve(self, peer):
 		"""
 		Moves a peer that has attempted to connect to this network instance from the 
 		unconfirmedList to peerList, where messages can be sent and received
 		"""
-		i = 0
-		while i < len(self.unconfirmedList):
-			peer = self.unconfirmedList[i]
-			input = self.printThis("y/n to add: " + str(peer) + " ", type = "input")
-			if input == "y":
-				self.peerList.append(peer)
-				self.unconfirmedList.remove(peer)
+		
+		self.unconfirmedList.remove(peer)
+		self.peerList.append(peer)
 
 	
 	def acceptor(self):
@@ -117,7 +85,7 @@ class Network(object):
 		user will not be forwarded to them
 		"""
 		clientSocket, clientAddress = self.server.accept() 
-		thisPeer = Peer(clientAddress[0],Socket = clientSocket)
+		thisPeer = Peer(clientAddress[0], socket = clientSocket)
 		if thisPeer not in self.unconfirmedList:
 			self.unconfirmedList.append(thisPeer)
 		
@@ -142,7 +110,7 @@ class Network(object):
 				messageStr = []
 				for peers in message:
 					messageStr.append(str(peers))
-				self.printThis("received peerlist: " + str(messageStr) + " from " + str((peers.ip, peers.port)))
+				print("received peerlist: " + str(messageStr) + " from " + str((peers.ip, peers.port)))
 			
 			else:
 				for peers in list(self.peerList):
@@ -151,13 +119,13 @@ class Network(object):
 							peers.Sock = None
 							peers.hasSock = None
 							if peers.name != None:
-								self.printThis(peers.name + " exited.")
+								print(peers.name + " exited.")
 							else:
-								self.printThis(str((peers.IP,peers.port)) + " exited.")
+								print(str((peers.IP,peers.port)) + " exited.")
 						elif peers.name != None:
-							self.printThis("from " + peers.name + ": " + str(message))
+							print("from " + peers.name + ": " + str(message))
 						else:
-							self.printThis("from " + str((peers.IP,peers.port)) + ": " + str(message))
+							print("from " + str((peers.IP,peers.port)) + ": " + str(message))
 				
 		
 	

Peer.py
@@ -14,14 +14,14 @@ class Peer(object):
 	name -- initialized as None, but can be added to give a peer object a unique identifier
 	"""
 	
-	def __init__(self, stringIP, intPort = None, Socket = None):
+	def __init__(self, stringIP, intPort = None, socket = None):
 		self.IP = stringIP
 		self.port = intPort
 			
 		# If a socket is provided, use it. Otherwise, document that.
 		#TODO Do we really need the bool hasSocket? Can't we just test for `self.socket is None`? 
-		if Socket is not None:
-			self.Sock = Socket
+		if socket is not None:
+			self.Sock = socket
 			self.hasSock = True 
 			
 		else:

WorkerThread.py
@@ -3,43 +3,43 @@ import time
 import socket
 
 class WorkerThread(Thread):
-  """ 
-  Class used for overriding the default thread constructor, and running each thread.
-  """
-  
-  def __init__(self, kind, network, debug = False, Stopper = False):
-    """ Initialize the class attributes as outlined here:
-    
-    * kind -- str -- Which kind of the thread to be used in determining which function it should initiate.
-    * network -- Network -- The network instance that the thread should operate on
-      This could be  useful is if in the future we would like to make a single program connect to several networks
-      at the same time (ie using one to connect to a network working on solving and another on blockchain.)    
-    * debug -- bool -- Whether or not to print debugging information.
-    * Stopper if set to true, any looping threads will stop
-    """
-    
-    Thread.__init__(self)
-    self.kind = kind
-    self.network = network
-    self.debug = debug
-    self.Stopper = Stopper
-    
-    # Make this thread a daemon so the terminal doesn't hang on keyboard interrupt.
-    self.setDaemon(True)
-
-  def run(self):
-    """ Determine which function this thread should run, and makes it happen it. """
-    if self.kind  == "manualClient":
-    	if self.debug:
-    		print("DEBUG: ManualClient thread is running.")
-    	self.network.manualClient()
-    while (not self.Stopper): 
-		if self.kind == "receiver":
+	"""
+	Class used for overriding the default thread constructor, and running each thread.
+	"""
+	
+	def __init__(self, kind, network, debug = False, Stopper = False):
+		""" Initialize the class attributes as outlined here:
+		
+		* kind -- str -- Which kind of the thread to be used in determining which function it should initiate.
+		* network -- Network -- The network instance that the thread should operate on
+			This could be useful is if in the future we would like to make a single program connect to several networks
+			at the same time (ie using one to connect to a network working on solving and another on blockchain.)
+		* debug -- bool -- Whether or not to print debugging information.
+		* Stopper if set to true, any looping threads will stop
+		"""
+		
+		Thread.__init__(self)
+		self.kind = kind
+		self.network = network
+		self.debug = debug
+		self.Stopper = Stopper
+		
+		# Make this thread a daemon so the terminal doesn't hang on keyboard interrupt.
+		self.setDaemon(True)
+	
+	def run(self):
+		""" Determine which function this thread should run, and makes it happen it. """
+		if self.kind == "manualClient":
 			if self.debug:
-				print("DEBUG: Receiver thread is running.")
-			self.network.receiver()
-		elif self.kind == "acceptor":
-			if self.debug:
-				print("DEBUG: Acceptor thread is running.")	
-			self.network.acceptor()
-		time.sleep(1)
+				print("DEBUG: ManualClient thread is running.")
+			self.network.manualClient()
+		while (not self.Stopper):
+			if self.kind == "receiver":
+				if self.debug:
+					print("DEBUG: Receiver thread is running.")
+				self.network.receiver()
+			elif self.kind == "acceptor":
+				if self.debug:
+					print("DEBUG: Acceptor thread is running.")
+				self.network.acceptor()
+			time.sleep(1)

commit bb664d5302daf47872f5722dab9168593d8724b1

Fix bugs that I introduced back in 789452ab9b158067bbafdc36949ec6f3078974d8

Main.py
@@ -79,10 +79,10 @@ def name():
 	Warning. Uses global variable myNetwork."""
 	
 	for peer in list(myNetwork.peerList):
-		print(str(peer) + " " + str(self.peerList.index(peers)))
+		print(str(peer) + " " + str(myNetwork.peerList.index(peer)))
 	index = int(raw_input("Please enter the index of the peer you would like to name: "))
 	name = raw_input("Please enter the new name: ")
-	self.peerList[int(index)].name = name
+	myNetwork.peerList[int(index)].name = name
 
 
 def approver():

Network.py
@@ -64,7 +64,7 @@ class Network(object):
 			newPeer = Peer(ip, port, socket = sock)
 			self.peerList.append(newPeer)
 			
-			newPeer.send(self.port)
+			newPeer.send(str(self.port))
 	
 	
 	